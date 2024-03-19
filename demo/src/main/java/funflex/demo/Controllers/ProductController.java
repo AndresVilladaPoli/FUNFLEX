1b6e50774be4618222df706dcf61f7e3a9258957
MateoEcheverri <mateo_echeverri82201@elpoli.edu.co>
2024-03-19 18:13:50 -0500

    Product Controller
    Add controller file for product entity

--- demo/src/main/java/funflex/demo/Controllers/ProductController.java
+++ demo/src/main/java/funflex/demo/Controllers/ProductController.java
@@ -1,5 +1,73 @@
 package funflex.demo.Controllers;
 
-public class ProductController {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import funflex.demo.Models.DAO.IProductDao;
+import funflex.demo.Models.Entity.Product;
+
+
+
+
+@Controller
+@SessionAttributes("product")
+
+public class ProductController{
+
+    @Autowired
+    private IProductDao productDao;
+
+    @RequestMapping(value = "/listproduct",method = RequestMethod.GET)
+
+    @GetMapping("/formproduct")
+    public String CreateProduct(Model model) 
+    {
+
+        Product product = new Product();
+
+        model.addAttribute("Title", "Products form");
+        model.addAttribute("product", product);
+
+
+        return "formproduct";
+    }
+
+    @RequestMapping(value = "/formproduct",method = RequestMethod.POST)
+    public String SaveProduct(Product product,SessionStatus status)
+    {
+       productDao.save(product);
+        status.setComplete();
+        return "redirect:listproduct";
+
+    }
+
+    @GetMapping("/formproduct/{IdProducto}")   
+    public String EditProduct(@PathVariable(value = "IdProducto") Long IdProducto ,Model model)
+    {
+
+        Product product = null;
+
+        if(IdProducto > 0)
+        {
+            product = productDao.findOne(IdProducto);
+        }
+        else
+        {
+            return "redirect:/listproduct";
+        }
+
+        model.addAttribute("Title", "Edit product");
+        model.addAttribute("product", product);
+
+
+        return "formproduct";
+    }
+
 }
