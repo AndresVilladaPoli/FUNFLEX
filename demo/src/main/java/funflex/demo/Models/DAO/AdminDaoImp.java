69ecd89a79b6eb14f04983dc187056cf1c120ff9
MateoEcheverri <mateo_echeverri82201@elpoli.edu.co>
2024-03-18 20:50:00 -0500

    DAO files
    Add Dao files for Admin entity

---
+++ demo/src/main/java/funflex/demo/Models/DAO/AdminDaoImp.java
@@ -0,0 +1,55 @@
+
+@Repository
+
+public Class AdminDaoImp implements IAdminDao{
+
+    @PersistenceContext
+    
+    private EntityManager em;
+
+    @SuppressWarnings("unchecked")
+    @Transactional(readOnly = true)
+    @Override
+    public List<Admin> findAll() 
+    { 
+      return em.createQuery("from Admin").getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void save(Admin admin)
+    {
+        if(admin.getIdAdmin() != null && admin.getIdAdmin() > 0)
+        {
+          em.merge(admin);
+        }
+        else
+        {
+          em.persist(admin); 
+        }
+        
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public Admin findOne(Long IdAdmin) {
+
+
+
+
+      return em.find(Admin.class, IdAdmin);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long IdAdmin) {
+      
+      Admin admin = findOne(IdAdmin);
+
+      em.remove(Admin);
+
+    }
+
+
+}
--- demo/src/main/java/funflex/demo/Models/DAO/IAdminDao.java
+++ demo/src/main/java/funflex/demo/Models/DAO/IAdminDao.java
@@ -1,3 +1,13 @@
-public class IAdminDao {
+import java.util.List;
+
+import funflex.demo.Models.Entity.Admin;
+
+public interface IAdminDao {
+    public List<Admin> findAll();       
     
+    public void save(Admin admin);
+
+    public Admin findOne(Long IdAdmin);
+
+    public void delete(Long IdAdmin);
 }
