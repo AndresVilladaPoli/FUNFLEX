88204d608d68685d9b3e333f1ed87c8479dd6385
Dayron <dayronrestrepo0212@gmail.com>
2024-03-27 21:18:57 -0500

    Add breadcrumb and pagination components

--- frontend/src/pages/inner-pages/portfolio/boxed-layout/PortfolioV11.jsx
+++ frontend/src/pages/inner-pages/portfolio/boxed-layout/PortfolioV11.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
 import Header from "../../../../components/portfolio/Header";
 import CopyrightFooter from "../../../../components/common/footer/CopyrightFooter";
 import { Link } from "react-router-dom";
-
-
+import Pagination from "../../shop/components/Pagination";
+import Breadcrumb from "../../shop/components/Breadcrumb";
 import Seo from "../../../../components/common/seo/Seo";
 
 const PortfolioV11 = () => {
@@ -13,7 +12,6 @@ const PortfolioV11 = () => {
       <Seo title="Products" />
 
       <Header />
-
 
       <div className="vcamp-text-block-one pt-180 md-pt-130">
         <div className="container">
@@ -28,16 +26,31 @@ const PortfolioV11 = () => {
                     The best entertainment products on the market.
                   </p>
                 </div>
+                <div className="breadcrumb-area">
+                  <div className="container">
+                    <div className="d-flex align-items-center justify-content-between">
+                      <nav className="breadcrumb-style-one mt-20">
+                        <Breadcrumb />
+                      </nav>
+                      <div className="share-dropdown mt-20">
+                        <div className="dropdown-menu dropdown-menu-end">
+                          <ul className="d-flex justify-content-between social-icon style-none">
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
       <div className="portfolio-gallery-eight portfolio-gallery-three border-top pt-130 pb-150 lg-pt-80 lg-pb-90">
-
       </div>
-
+      <div className="page-pagination-one pt-30 ml-550">
+        <Pagination />
+      </div>
       <footer className="vcamp-footer-two border-top pt-130 lg-pt-100">
         <div className="container">
           <div className="row">
@@ -47,28 +60,21 @@ const PortfolioV11 = () => {
                   <img
                     src={
                       require("../../../../assets/images/logo/funflexblanco.png")
-                        
                     }
                     alt="brand"
                   />
                 </Link>
               </div>
             </div>
-
-         
           </div>
         </div>
-
-
         <div className="container">
           <div className="bottom-footer">
             <CopyrightFooter />
           </div>
         </div>
-
       </footer>
     </div>
   );
 };
-
 export default PortfolioV11;
