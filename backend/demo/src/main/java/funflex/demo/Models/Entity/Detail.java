aa6278d8f2d7f087fcaa890665ddb837aed6feba
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-03-28 21:01:31 -0500

    (backend) update entities
    update the content of the 'Detail' and 'Sale' classes

--- backend/demo/src/main/java/funflex/demo/Models/Entity/Detail.java
+++ backend/demo/src/main/java/funflex/demo/Models/Entity/Detail.java
@@ -1,3 +1,47 @@
-public class Detail {
+package main.java.funflex.demo.Models.Entity;
 
+import funflex.demo.Models.Entity.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Detail")
+
+public class Detail {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long IdDetail;
+
+    private Product product;
+    private Sale sale;
+    private int amount;
+
+    public long getIdDetail() {
+        return IdDetail;
+    }
+    public void setIdDetail(long idDetail) {
+        IdDetail = idDetail;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Sale getSale() {
+        return sale;
+    }
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
--- backend/demo/src/main/java/funflex/demo/Models/Entity/Sale.java
+++ backend/demo/src/main/java/funflex/demo/Models/Entity/Sale.java
@@ -1,3 +1,47 @@
+package main.java.funflex.demo.Models.Entity;
+
+import funflex.demo.Models.Entity.Customer;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Sale")
 public class Sale {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long IdSale;
+
+
+    private Date date;
+    private Double Total;
+    private Customer Client;
+
+    public long getIdSale() {
+        return IdSale;
+    }
+    public void setIdSale(long idSale) {
+        IdSale = idSale;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public Double getTotal() {
+        return Total;
+    }
+    public void setTotal(Double total) {
+        Total = total;
+    }
+    public Customer getClient() {
+        return Client;
+    }
+    public void setClient(Customer client) {
+        Client = client;
+    }
 }
