dd2926fdae30c6c398d82d6a1444010437a00a20
MateoEcheverri <mateo_echeverri82201@elpoli.edu.co>
2024-03-30 14:01:51 -0500

    Login (backend)
    Add Login file for admin in Controllers folder

--- backend/demo/src/main/java/funflex/demo/Controllers/AdminLoginController.java
+++ backend/demo/src/main/java/funflex/demo/Controllers/AdminLoginController.java
@@ -1,7 +1,41 @@
 package main.java.funflex.demo.Controllers;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/Adminlogin")
 public class AdminLoginController {
     
+    private AdminService adminService;
+
+	public AdminLoginController(AdminService adminService) {
+		this.adminService = adminService;
+	}
+	
+	@ModelAttribute("admin")
+	public AdminLoginDTO returnNewAdminLoginDTO() {
+		return new AdminLoginDTO();
+	}
+
+	@GetMapping
+	public String ShowLoginForm() {
+		return "AdminLogin"; 
+	}
+	
+	
+	@PostMapping
+	public String ProcessLoginForm(@ModelAttribute("admin") AdminLoginDTO loginDTO) {
+		boolean loginSuccessful = adminService.login(loginDTO);
+		if (loginSuccessful) {
+			return "redirect:/Successfull"; 
+		} else {
+			return "redirect:/Adminlogin?error"; 
+		}
+	}
 }
