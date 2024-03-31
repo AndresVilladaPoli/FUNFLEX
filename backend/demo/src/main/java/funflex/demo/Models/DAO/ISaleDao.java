3a3db95e461daad82f3563261515f756cba008fe
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-03-30 21:12:33 -0500

    (backend) add DAO interfaces
    add the 'Sale', 'Detail' and 'ShoppingCart' interfaces in DAO folder

---
+++ backend/demo/src/main/java/funflex/demo/Models/DAO/ISaleDao.java
@@ -0,0 +1,9 @@
+package main.java.funflex.demo.Models.DAO;
+
+import funflex.demo.Models.Entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public class ISaleDao extends JpaRepository<Sale, long[]> {
+    List<Sale> findByCustomerId(long IdCustomer);
+}
