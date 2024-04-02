5e772c21ec9e2797f9cfe99ba52792e114a1f138
MateoEcheverri <mateo_echeverri82201@elpoli.edu.co>
2024-04-02 15:59:12 -0500

    Login
    Change login files into one

--- backend/demo/src/main/java/funflex/demo/Controllers/LoginController.java
+++ backend/demo/src/main/java/funflex/demo/Controllers/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class LoginController {
     
     @Autowired
@@ -16,16 +16,39 @@ public class LoginController {
     private AdminService adminService;
     
     @GetMapping("/login")
-    public String SignUp() {
+    public String showLoginForm(Model model) {
+        model.addAttribute("customer", new CustomerLoginDTO());
+        model.addAttribute("admin", new AdminLoginDTO());
         return "login";
     }
     
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute("customer") CustomerLoginDTO customerLoginDTO, 
+                                   @ModelAttribute("admin") AdminLoginDTO adminLoginDTO) {
+        String email = "";
+        if (customerLoginDTO.getEmail() != null) {
+            email = customerLoginDTO.getEmail();
+        } else if (adminLoginDTO.getAdminEmail() != null) {
+            email = adminLoginDTO.getAdminEmail();
+        }
+
+        if (email.endsWith("gmail.com")) {
+            if (customerService.login(customerLoginDTO)) {
+                return "redirect:/customerSuccess"; 
+            }
+        } else if (email.endsWith("funflex.com")) {
+            if (adminService.login(adminLoginDTO)) {
+                return "redirect:/adminSuccess"; 
+            }
+        }
+        
+        return "redirect:/login?error";
+    }
+    
     @GetMapping("/")
-    public String ViewLogin(Model model) {
-        model.addAttribute("customers", customerServicio.listCustomer());
-        model.addAttribute("admins", adminServicio.listAdmin());
+    public String viewLogin(Model model) {
+        model.addAttribute("customers", customerService.findAll());
+        model.addAttribute("admins", adminService.findAll());
         return "index";
     }
 }
-
-
