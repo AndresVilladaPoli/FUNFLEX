6bd69b19c89f03bed6ce018ef9d81dbc9419eaf6
Dayron <dayronrestrepo0212@gmail.com>
2024-04-08 16:12:13 -0500

    Add SignupClient component and form signup-client

---
+++ frontend/src/components/common/form/SignupClientForm.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [password, setPassword] = useState(false);
+  const passwordHandler = () => setPassword(!password);
+
+  const [confirmPassword, setConfirmPassword] = useState(false);
+  const confirmPasswordHandler = () => setConfirmPassword(!confirmPassword);
+
+  return (
+    <form>
+      <div className="row">
+        <div className="col-12">
+          <div className="input-group-meta mb-25">
+            <label>Name*</label>
+            <input type="text" placeholder="Pedro" required />
+          </div>
+        </div>
+        <div className="col-12">
+          <div className="input-group-meta mb-25">
+            <label>User*</label>
+            <input type="text" placeholder="Pepe" required />
+          </div>
+        </div>
+
+        <div className="col-12">
+          <div className="input-group-meta mb-30">
+            <label>Email*</label>
+            <input type="email" placeholder="pedropepepe@gmail.com" required />
+          </div>
+        </div>
+
+        <div className="col-12">
+          <div className="input-group-meta mb-30">
+            <label>Address</label>
+            <input type="text" placeholder="Street Avenue"/>
+          </div>
+        </div>
+
+        <div className="col-12">
+          <div className="input-group-meta mb-30">
+            <label>Telephone*</label>
+            <input
+              type="tel"
+              pattern="[0-9]{10}"
+              title="Enter a 10-digit identification number"
+              placeholder="1234567891"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="col-12">
+          <div className="input-group-meta mb-25">
+            <label>Password*</label>
+            <input
+              type={password ? " text" : "password"}
+              placeholder="Enter Password"
+              className="pass_log_id"
+              required
+            />
+            <span className="placeholder_icon">
+              <span
+                className={password ? " eye-slash" : "passVicon"}
+                onClick={passwordHandler}
+              >
+                <img
+                  src={
+                    require("../../../assets/images/icon/icon_67.svg").default
+                  }
+                  alt="icon"
+                />
+              </span>
+            </span>
+          </div>
+        </div>
+
+        <div className="col-12">
+          <div className="input-group-meta mb-25">
+            <label>Confirm Password*</label>
+            <input
+              type={confirmPassword ? "text" : "password"}
+              placeholder="Enter Password"
+              className="pass_log_id"
+              required
+            />
+            <span className="placeholder_icon">
+              <span
+                className={confirmPassword ? "eye-slash" : "passVicon"}
+                onClick={confirmPasswordHandler}
+              >
+                <img
+                  src={
+                    require("../../../assets/images/icon/icon_67.svg").default
+                  }
+                  alt="icon"
+                />
+              </span>
+            </span>
+          </div>
+        </div>
+
+    
+
+        <div className="col-12">
+          <button className="theme-btn-one w-100 mt-50 mb-50">Sign Up</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Signup;
--- frontend/src/pages/inner-pages/shop/shop-elements/signUpClient.jsx
+++ frontend/src/pages/inner-pages/shop/shop-elements/signUpClient.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import img1 from "../../../../assets/images/assets/sign-in img.png";
+import img1 from "../../../../assets/images/assets/SignUp.jpg";
 import img2 from "../../../../assets/images/logo/funflexnegro.png"
-import LoginForm from "../../../../components/common/form/LoginForm";
+import SignupForm from "../../../../components/common/form/SignupClientForm";
 import Seo from "../../../../components/common/seo/Seo";
 
-const SignIn = () => {
+const SignUp = () => {
   return (
     <>
-      <Seo title="Sign In" />
+      <Seo title="Sign Up Client" />
 
       <div className="user-data-page clearfix d-md-flex">
         <div
           className="illustration-wrapper d-none d-md-flex align-items-center justify-content-between flex-column"
           style={{ background: "#F9F5F0" }}
         >
-          <div className="row">
+         <div className="row">
             <div className="col-xxl-8 col-xl-11 m-auto">
               <blockquote>
                 "Entertainment is the fuel that ignites our imagination and allows us to discover new realities within ourselves."
@@ -25,15 +25,15 @@ const SignIn = () => {
           <div className="illustration-holder">
             <img
               src={img1}
-              alt="illustration"
-              className="illustration ms-auto"
+              alt="illustrationad"
+              className="illustrationad"
             />
           </div>
         </div>
 
         <div className="form-wrapper">
           <div className="d-flex justify-content-between align-items-center">
-            
+      
             <Link to="/" className="go-back-button">
               Go to home
             </Link>
@@ -41,29 +41,29 @@ const SignIn = () => {
 
           <div className="user-data-form mt-80 lg-mt-50">
             <h2 className="font-recoleta">
-              Hi <span>buddy</span>, welcome <br /> Back!
+              Hi buddy, welcome <br /> Back!
             </h2>
             <p className="header-info pt-20 pb-50 lg-pb-30">
-              Still don't have an account? <Link to="/sign-up-client">Sign up</Link>
+              Have an account? Login <Link to="/sign-in"> here</Link>
             </p>
-            <LoginForm />
+
+            <SignupForm />
+            <p className="text-center copyright-text">
+              Copyright @2024.
+            </p>
           </div>
         </div>
-
       </div>
-      <p className="hoo">
-              Copyright @2024.
-      </p>
-
+     
       <div className="funfleximg">
             <img
               src={img2}
               alt="illustration"
-              className="illustration ms-auto"
+              className="illustration"
             />
           </div>
     </>
   );
 };
 
-export default SignIn;
+export default SignUp;
